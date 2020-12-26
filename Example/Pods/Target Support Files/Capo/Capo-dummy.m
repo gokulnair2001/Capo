41e6df622b06333edc293cafea52d3cb1bb61507
“gokulnair2001” <gokulnair.2001@gmail.com>
2020-12-26 14:05:23 +0530

    initial commit with cocoa pod template

---
+++ Example/Pods/Target Support Files/Capo/Capo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Capo : NSObject
+@end
+@implementation PodsDummy_Capo
+@end
---
+++ Source/ButtonTuner.swift
@@ -0,0 +1,8 @@
+//
+//  ButtonTuner.swift
+//  Capo
+//
+//  Created by Gokul Nair on 26/12/20.
+//
+
+import Foundation
